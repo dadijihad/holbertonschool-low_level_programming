ac9796950f50057fa147331c8987e60a117d3942
dadijihad <jihadedahij9292@gmail.com>
2024-02-23 21:52:24 +0000

    vaiable

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
 
-void printAlphabet(void);
-
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    printAlphabet();
-    putchar('\n');  // Imprimer un caractère de nouvelle ligne après l'alphabet
-    return 0;
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
 }
 
-void printAlphabet(void)
-{
-    char low;
 
-    for (low = 'a'; low <= 'z'; low++)
-    {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
