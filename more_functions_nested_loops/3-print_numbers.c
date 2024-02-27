f2207969d5c31abeae5c235b8a15830e6090ec8c
dadijihad <jihadedahij9292@gmail.com>
2024-02-27 16:18:50 +0000

    exerce

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return:0.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}	
+	_putchar('\n');
+}	
+
