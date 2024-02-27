4922075495252d6f6fc8b13d43f50452ee969f0b
dadijihad <jihadedahij9292@gmail.com>
2024-02-27 16:24:42 +0000

    exece

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return:0.
+ * print_numbers - prints the numbers, from 0 to 9
  */
 void print_numbers(void)
 {
@@ -11,7 +9,6 @@ void print_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
-	}	
+	}
 	_putchar('\n');
-}	
-
+}
