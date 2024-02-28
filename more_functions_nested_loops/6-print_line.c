39d9dd8d8659b19721cbdc19c2a5989a43dcb13a
dadijihad <jihadedahij9292@gmail.com>
2024-02-28 21:19:36 +0000

    line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_line - function that checks for uppercase character.
+ * print_line - _putchar.c.
  * @n: is the integer for the paramaters of my function
  */
 void print_line(int n)
