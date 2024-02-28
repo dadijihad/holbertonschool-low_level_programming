3a211fc0b31f88db29c036ea24c62fd2d10baae4
dadijihad <jihadedahij9292@gmail.com>
2024-02-28 19:54:31 +0000

    exece

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * print_most_numbers - numbers from '0' to '9' , excluding '2' and '4'. 
+ * print_most_numbers - Prints numbers from '0' to '9', excluding '2' and '4'.
  */
 void print_most_numbers(void)
 {
