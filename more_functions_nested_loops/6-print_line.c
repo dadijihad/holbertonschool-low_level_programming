af6da8b8200fc7d0b15bb1d26f0f92e08bedeb88
dadijihad <jihadedahij9292@gmail.com>
2024-02-29 15:05:35 +0000

    exerce

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * main - prints numbers between 0 to 9.
- *
- * Return: 0.
- */
-int main(void)
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
+void print_line(int n)
 {
-	int n;
-
-	for (n = 0; n < 10; n++)
-		{
-			putchar(n +'0');
-		}
-		_putchar('\n');
-		return (0);
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
