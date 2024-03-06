2fcebb33193c851fe2a79b689ab00798d1af6717
dadijihad <jihadedahij9292@gmail.com>
2024-03-06 14:07:26 +0100

    strchr

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,12 @@
 #include "main.h"
 /**
-*_strspn-retourns the number of bytes in the initial segment*@s:segment tageted
-*@accept:reference bytes
-*returns the number of bytes in the initial
+*_strspn - search the number of bytes in the initial
+* segment of s which consist only of bytes from accept
+*@s:segment targeted
+*@accept:reference bytes container
+*
+*Return:returns the number of bytes in the initial
+* segment of s which consist only of bytes from accept
 */
 unsigned int _strspn(char *s, char *accept)
 {
