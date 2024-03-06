8a94ce30964f756cfa039997c281f50cb217cbc1
dadijihad <jihadedahij9292@gmail.com>
2024-03-06 21:26:55 +0100

    code

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
-*_strstr-the strstr() function finds the first occurence
-*of the substring needle in the sting haystack
-*the terminating null bytes (\0) are not compared
-*@haystack: string were the search is made
-*@needle: string whose occurence is searched in haystack 
-*Retun: Returns a pointer to the beginning of the located
+*_strstr - The _strstr() function finds the first occurrence
+* of the substring needle in the string haystack.
+* The terminating null bytes (\0) are not compared
+*@haystack: string where the search is made
+*@needle: string whose occurence is searched in haystack
+*Return:Returns a pointer to the beginning of the located
+* substring, or NULL if the substring is not found.
 */
 
 char *_strstr(char *haystack, char *needle)
