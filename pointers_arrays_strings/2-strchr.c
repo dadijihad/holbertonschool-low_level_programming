0e0a4d08bbb6f591a81daf9e212877902f229594
dadijihad <jihadedahij9292@gmail.com>
2024-03-06 12:05:42 +0100

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,10 +9,14 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-
-	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++);
+	i = 0;
+	while ((s[i] != c) && (s[i] != '\0'));
+{
+	 i++;
 	if (s[i] == c)
 		return (s + i);
+
 	else
 		return (NULL);
+}
 }
