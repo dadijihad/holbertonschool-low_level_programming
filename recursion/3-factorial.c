7392246cda99d55e71b19dcb4c2e936f2839d130
dadijihad <jihadedahij9292@gmail.com>
2024-03-07 16:34:07 +0100

    recurrsion

---
+++ recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *factorial - return the faactorial of a given number 
+ *@n: the number of factorial 
+ *Return: the factorial
+ */
+int factorial(int n)
+{
+    if (n < 0) {
+        return -1; 
+    } else if (n == 0) {
+        return 1;  
+    } else {
+        return n * factorial(n - 1);
+    }
+}
