f4c4f9548aac52d3c0484f4379f68debee170a4f
dadijihad <jihadedahij9292@gmail.com>
2024-03-08 00:02:46 +0100

    argc

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*main - prints multiplication 
+*main - prints multiplication
 *@argc: number of arguments
 *@argv: array of arguments
 *Return: returns 0
