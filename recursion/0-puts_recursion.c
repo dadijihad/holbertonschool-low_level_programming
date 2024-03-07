f1ff84ef7e1e009a55240b27d8d19d1359d43a7c
dadijihad <jihadedahij9292@gmail.com>
2024-03-07 10:53:20 +0100

    recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -3,15 +3,15 @@
  * _puts_recursion - print a string - followed by a new line
  *@s: printed
  *Retusn: Always 0
-*/
- void _puts_recursion(char *s)
- { 
+ */
+void _puts_recursion(char *s)
+{ 
 
-	 if(*s== '\0')
-	 _putchar('\n');
- else
- 	{
-	 _putchar(s[0]);
-	 _puts_recursion(s + 1);
-	 }	
- }
+	if(*s== '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
+	}	
+}
