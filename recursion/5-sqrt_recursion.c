a851efa1d3c346d97932202b6c8726e1122a5771
dadijihad <jihadedahij9292@gmail.com>
2024-03-09 23:37:49 +0100

    recusion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
- * checker - check the code
- *
- * @racine_probable: number is squared and compared against base
- * @valeur_test: base number to check
- *
- * Return: Always puissance.
- */
+*checker - check the code
+*@racine_probable: number is squared and compared against base
+*@valeur_test: base number to check
+*Return: Always puissance.
+*/
 int checker(int racine_probable, int valeur_test)
 {
         if (racine_probable * racine_probable == valeur_test)
@@ -16,10 +14,10 @@ int checker(int racine_probable, int valeur_test)
         return (checker(racine_probable + 1, valeur_test));
 }
 /**
- * _sqrt_recursion - return the natural square root of a number n.
- * @n: number to check for square roots.
- * Return: the natural square root of number n
- */
+*_sqrt_recursion - return the natural square root of
+*@n: number to check for square roots.
+*Return: the natural square root of number n
+*/
 int _sqrt_recursion(int n)
 {
         return (checker(1, n));
