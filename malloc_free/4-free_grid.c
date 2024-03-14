e267ebe286ed27b8287471cbd87a3070304a7ce6
dadijihad <jihadedahij9292@gmail.com>
2024-03-14 21:42:37 +0100

    alloc

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*free_grid - that free a 2 dimensional
+*@grid: the adress of the two dimensional grid
+*@height: height of grid 
+*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+} 
