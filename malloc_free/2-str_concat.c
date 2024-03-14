6bc60722729d49e0da4403ca96aad02ff6075d68
dadijihad <jihadedahij9292@gmail.com>
2024-03-14 11:48:15 +0100

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
 
-	concat_str = malloc(sizeof(char) * len);
+	concat_str = malloc(sizeof(char) *( len + 1));
 
 	if (concat_str == NULL)
 		return (NULL);
