c817ae2156d4ee8192147aca24d815227b7e1533
dadijihad <jihadedahij9292@gmail.com>
2024-03-14 11:39:26 +0100

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -4,11 +4,12 @@
 *@s1:First string
 *@s2:Second string
 *Return: a newly allocated space in memory
-*/ 
+*/
+ 
 char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
-	int index, concat_index = 0,  len = 0;
+	int i , j = 0, len = 0;
 
 	if (s1 == NULL)
 		s1 = "";
@@ -16,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	for (index = 0; s1[index] || s2[index]; index++)
+	for (i = 0; s1[i] || s2[i]; i++)
 		len++;
 
 	concat_str = malloc(sizeof(char) * len);
@@ -24,11 +25,11 @@ char *str_concat(char *s1, char *s2)
 	if (concat_str == NULL)
 		return (NULL);
 
-	for (index = 0; s1[index]; index++)
-		concat_str[concat_index++] = s1[index];
+	for (i = 0; s1[i]; i++)
+		concat_str[j++] = s1[i];
 
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s2[index];
+	for (i = 0; s2[i]; i++)
+		concat_str[j++] = s2[i];
 
 	return (concat_str);
 }
