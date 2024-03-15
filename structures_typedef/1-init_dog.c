c0cf67063046c698744ed2a970ff0dbc5ed68213
dadijihad <jihadedahij9292@gmail.com>
2024-03-15 15:47:49 +0100

    exerce

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *init_dog - inits a variable of
+ *@d: dog identification
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner's name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
