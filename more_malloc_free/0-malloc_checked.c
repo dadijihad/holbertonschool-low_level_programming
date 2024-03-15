cd0d31cb482c1421745dd592a969dbae07264237
dadijihad <jihadedahij9292@gmail.com>
2024-03-15 09:19:10 +0100

    malloc

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*malloc-checked - allocates memory using
-*@b: size of the memory 
-*Return: pointer to the address of the memory block
-*/
+ *malloc_checked - allocates memory using
+ *@b: size of the memory
+ *Return: pointer to the address of the memory block
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *block;
