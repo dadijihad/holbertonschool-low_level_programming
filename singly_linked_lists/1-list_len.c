1db6a001e4e7093dfbc09e9d26f5e06d08d0c197
dadijihad <jihadedahij9292@gmail.com>
2024-04-04 12:55:07 +0200

    list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,17 +1,9 @@
 #include "lists.h"
 
 /**
-*list_len - finds the numbers of elements in a linked list *@h: pointer to the list 
+* print_list - returns the numbe of elementsi
+* @h:pointer to the list
 *Return: the number of elements 
-*/
-
-#include "lists.h"
-
-/**
- * list_len - finds the number of elements in a linked list
- *@h: pointer to the list
- *
- *Return: the number of elements
 */
 
 size_t list_len(const list_t *h)
