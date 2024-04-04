09b51837560089df893bff83bb15d56ada955290
dadijihad <jihadedahij9292@gmail.com>
2024-04-04 14:09:20 +0200

    list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
-* print_list - returns the numbe of elementsi
-* @h:pointer to the list
-*Return: the number of elements 
+*print_list - returns the number of elements
+*@h:pointer to the list
+*Return: the number of elements
 */
 
 size_t list_len(const list_t *h)
